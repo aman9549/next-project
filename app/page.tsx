3467837dc8f17c386ca6c532bb5ba9a5bf486a2d
aman9549 <amankuls007@gmail.com>
2025-11-23 21:47:30 +0530

    created navbar and event cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
-import React from 'react'
+
+const events = [
+  {
+    image:'/images/event1.png',
+    title:'Event 1'
+  },
+  {
+    image:'/images/event2.png',
+    title:'Event 2'
+  }
+]
 
 const Page = () => {
   return (
@@ -8,6 +19,22 @@ const Page = () => {
         <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
             <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
             <ExploreBtn />
+
+            <div className="mt-20 space-y-2">
+              <h3>Featured Events</h3>
+
+              <ul className='events'>
+                {
+                  events.map((event)=>{
+                    return(
+                      <li key={event.title}>
+                        <EventCard {...event} />
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            </div>
    </section>
     </>
   )
